0511deccde9aa3e3c60bf511ddbe26a2775a2c07
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-06-18 22:29:07 -0300

    trabalhando com configurações do projeto, instalação prettier, eslint e editorconfig

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React, { useState, useEffect } from "react";
+
 const App = () => {
 
+    const [usuarios, setUsuarios] = useState(
+        ["Vinicius", "Junior", "Carlos"])
+        
+        useEffect(() =>{
+
+            setTimeout(() => {
+
+                setUsuarios([...usuarios, "Carlos"]);
+
+            }, 5000);
+
+        }, []);
+        
     return (
 
         <div className="App">
             <h1>Hello APP</h1>
+            {usuarios.map((item) => (
+            <p>{item}</p>
+            
+            ))}
         </div>
 
 
